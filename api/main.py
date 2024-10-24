371e667093004c40c39c3c5a296522baa935675d
Fellipe <fellipe_sdp@hotmail.com>
2024-10-24 08:45:12 -0300

    gitignore

--- api/main.py
+++ api/main.py
@@ -1,6 +1,15 @@
 from fastapi import APIRouter
-
-from api.routes.sign_up import auth_router
+from api.routes.login import login_router
+from api.routes.sign_up import signup_router
+from fastapi_jwt_auth import AuthJWT
+from sql.schemas import settings
 
 api_router = APIRouter()
-api_router.include_router(auth_router)
+
+@AuthJWT.load_config
+def get_config():
+    return settings()
+
+api_router.include_router(signup_router)
+api_router.include_router(login_router)
+
